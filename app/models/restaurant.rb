b2e7b9bcf9965f62cbe6219ee76e64848df165b4
Enrique Naguiat <enzo_naguiat@yahoo.com>
2017-10-12 00:41:32 +0900

    completed rake objectives

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
 end
